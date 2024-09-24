2a122e3f96a280f8b1e2cb1d03378ea16c44ad7c
Rhoda <rhodachela@gmail.com>
2024-09-24 16:06:42 +0300

    Removed HCIA-AI directory and quest1.py file, added question files

---
+++ HCIA-Python/quest3.py
@@ -0,0 +1,5 @@
+num1 = 20
+num2 = 30
+
+result = num1 + num2
+print(result)
---
+++ HCIA-Python/quest4.py
@@ -0,0 +1,7 @@
+my_shopping_list = (5,)
+print(my_shopping_list)
+
+my_list = (43, 90, 7, 45, 99)
+
+for items in my_list:
+    print(items)

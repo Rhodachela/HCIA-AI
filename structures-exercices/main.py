03de0309b188b3c8c3607aefef38d0f96c2a8410
Rhoda <rhodachela@gmail.com>
2024-09-24 19:58:33 +0300

    Practice exercises

---
+++ structures-exercices/main.py
@@ -0,0 +1,21 @@
+#main.py (Program)
+
+#import math_functions
+
+#result1 = math_functions.add(57, 23)
+#print("Addition: ", result1)
+#result2 = math_functions.subtract(99, 39)
+#print("Subtraction: ", result2)
+
+import calculator
+sum = calculator.addition(70, 30, 5)
+print(sum)
+
+sum2 = calculator.subtra(70, 30, 5)
+print(sum2)
+
+sum3 = calculator.mul(70, 30, 5)
+print(sum3)
+
+div = calculator.division (70, 30)
+print(div)

7dda8dfc9a126101b3fb55078bbb6f9ceb0f7a3f
Rhoda <rhodachela@gmail.com>
2024-09-24 21:58:47 +0300

    Namespace exploration instruction

---
+++ structures-exercices/ex2.py
@@ -0,0 +1,11 @@
+def Mul(x, y):
+    count = x * y
+    return count
+res = Mul(75, 25)
+print(f"Multiplication result is {res}")
+
+def Add(x, z):
+    count = x + z
+    return count
+result = Add(75, 25)
+print(f"The total is {result}")

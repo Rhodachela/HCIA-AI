ceec1b7198ed305c66c6efc14a8e65a68705aff5
Rhoda <rhodachela@gmail.com>
2024-09-24 19:57:39 +0300

    Creating and Using modules

---
+++ structures-exercices/calculator.py
@@ -0,0 +1,16 @@
+
+def addition( x, y, z):
+    result1 = x + y + z
+    return result1
+
+def subtra(x, y, z):
+    result2 = x - y -z
+    return result2
+
+def mul(x, y, z):
+    product = x * y * z
+    return product
+
+def division(x, y):
+    result3 = x / y
+    return result3
